d52f2e6e6a2f209b37f6dcd7d5f5bb570d8d7434
Nicolas DI PRIMA <nicolas@di-prima.fr>
2013-12-17 20:06:40 +0000

    use Hit library to check the presence of a repository then print them

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,7 +1,32 @@
 {-# LANGUAGE TupleSections, OverloadedStrings #-}
+{-# LANGUAGE DoAndIfThenElse #-}
 module Handler.Home where
 
 import Import
+
+import Data.Maybe
+import Data.Git
+import Data.Text
+import System.Directory
+
+import Filesystem.Path as FSP
+import Filesystem.Path.Rules
+
+toPath :: String -> String -> FSP.FilePath
+toPath path dir = (decodeString posix_ghc704 path) </> (decodeString posix_ghc704 dir) </> ".git"
+
+insertProject :: String -> String -> IO (Maybe String)
+insertProject path repo = do
+  isHitProject <- isRepo $ toPath path repo
+  if (isHitProject) then
+    return $ Just repo
+  else do
+    return Nothing
+
+getProjectsName :: String -> IO [Maybe String]
+getProjectsName path = do
+  contents <- getDirectoryContents path
+  mapM (insertProject path) contents
 
 -- This is a handler function for the GET request method on the HomeR
 -- resource pattern. All of your resource patterns are defined in
@@ -15,6 +40,7 @@ getHomeR = do
     maid <- maybeAuthId
     defaultLayout $ do
         aDomId <- newIdent
-        setTitle "LAMIH::home"
+        projectsList <- liftIO $ getProjectsName "/home/nicolas/work/yesod"
+        setTitle "HitWeb::home"
         $(widgetFile "default-head")
         $(widgetFile "homepage")
---
+++ Handler/Project.hs
@@ -0,0 +1,20 @@
+module Handler.Project where
+
+import Import
+
+import Data.Text
+import Data.Git
+import Filesystem.Path as FSP
+import Filesystem.Path.Rules
+
+toPath :: String -> String -> FSP.FilePath
+toPath path dir = (decodeString posix_ghc704 path) </> (decodeString posix_ghc704 dir) </> ".git"
+
+getProjectR :: Text -> Handler Html
+getProjectR  projectName = do
+    isGitProject <- liftIO $ isRepo $ toPath "/home/nicolas/work/yesod" $ unpack projectName
+    defaultLayout $ do
+        aDomId <- newIdent
+        setTitle $ toHtml $ "HitWeb::project::" ++ (unpack projectName)
+        $(widgetFile "default-head")
+        $(widgetFile "project")
