a8a0ff0b89efebcd1b64f9c641eb013492f00212
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-01-27 07:39:27 +0000

    Implement the diff page

--- Handler/ProjectShowDiff.hs
+++ Handler/ProjectShowDiff.hs
@@ -2,9 +2,16 @@ module Handler.ProjectShowDiff where
 
 import Import
 
+import Data.List as L
 import Data.Git
+import Data.Git.Diff
+import Data.Git.Ref
+import Data.Algorithm.Patience (Item(..))
 
 import Data.Text as T
+
+import Data.ByteString.Char8 as BC (unpack)
+import Data.ByteString.Lazy.Char8 as BL (unpack)
 
 import Filesystem.Path as FSP
 import Filesystem.Path.Rules as FSP
@@ -21,7 +28,11 @@ getProjectShowDiffR projectName oldRef newRef = do
     defaultLayout $ do
         isHitProject <- liftIO $ isRepo (projectPathF </> ".git")
         setTitle $ toHtml $ "Hit - " ++ (T.unpack projectName)
+        addScriptRemote "http://code.jquery.com/ui/1.10.4/jquery-ui.js"
         $(widgetFile "default-head")
         if not isHitProject
             then error $ "No such project: " ++ (T.unpack projectName)
-            else $(widgetFile "project-show-diff")
+            else do diffList <- liftIO $ withRepo (projectPathF </> ".git") $ getDiff (fromHexString $ T.unpack oldRef) (fromHexString $ T.unpack newRef)
+                    $(widgetFile "project-show-diff")
+                    let infiniteList = [1..] :: [Integer]
+                    $(widgetFile "project-show-diff-file")
