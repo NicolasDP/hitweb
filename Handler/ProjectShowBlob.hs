15be9fc4308f6fc973b73c63eae1d0ba86dc5bba
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-01-27 21:03:11 +0000

    implement show Tree and Blob handlers

---
+++ Handler/ProjectShowBlob.hs
@@ -0,0 +1,40 @@
+module Handler.ProjectShowBlob where
+
+import Import
+
+import Data.Git
+import Data.Git.Ref
+import Data.Git.Repository
+import Data.Git.Storage
+import Data.Git.Storage.Object
+import Data.Text as T (pack,unpack,concat)
+
+import Data.ByteString.Char8 as BC (unpack)
+import Data.ByteString.Lazy.Char8 as BL
+
+import Filesystem.Path as FSP
+import Filesystem.Path.Rules as FSP
+
+toPath :: Text -> FSP.FilePath
+toPath path = FSP.decodeString posix_ghc704 $ T.unpack path
+
+myGetBlobMaybe :: Ref -> Git -> IO (Maybe ObjectInfo)
+myGetBlobMaybe ref git = getObjectRaw git ref True
+
+getProjectShowBlobR :: Text -> Text -> Handler Html
+getProjectShowBlobR projectName ref = do
+    extra <- getExtra
+    let projectsDir = extraProjectsDir extra
+    let projectPathT = T.concat [projectsDir,T.pack "/",projectName]
+    let projectPathF = toPath projectPathT
+    defaultLayout $ do
+        identityTree <- newIdent
+        isHitProject <- liftIO $ isRepo (projectPathF </> ".git")
+        setTitle $ toHtml $ "Hit - " ++ (T.unpack projectName)
+        $(widgetFile "default-head")
+        if not isHitProject
+            then error $ "No such project: " ++ (T.unpack projectName)
+            else do
+                let identityNew = T.unpack identityTree
+                fileContent <- liftIO $ withRepo (projectPathF </> ".git") $ myGetBlobMaybe $ fromHexString $ T.unpack ref
+                $(widgetFile "project-show-blob")
---
+++ Handler/ProjectShowTree.hs
@@ -0,0 +1,41 @@
+module Handler.ProjectShowTree where
+
+import Import
+
+import Data.Git
+import Data.Git.Ref
+import Data.Git.Repository
+import Data.Text as T (pack,unpack,concat)
+
+import Data.ByteString.Char8 as BC (unpack)
+
+import Filesystem.Path as FSP
+import Filesystem.Path.Rules as FSP
+
+toPath :: Text -> FSP.FilePath
+toPath path = FSP.decodeString posix_ghc704 $ T.unpack path
+
+myGetTreeMaybe :: Ref -> Git -> IO HTree
+myGetTreeMaybe ref git = do
+    tree <- getTreeMaybe git ref
+    case tree of
+        Just t  -> buildHTree git t
+        Nothing -> return []
+
+getProjectShowTreeR :: Text -> Text -> Handler Html
+getProjectShowTreeR projectName ref = do
+    extra <- getExtra
+    let projectsDir = extraProjectsDir extra
+    let projectPathT = T.concat [projectsDir,T.pack "/",projectName]
+    let projectPathF = toPath projectPathT
+    defaultLayout $ do
+        identityTree <- newIdent
+        isHitProject <- liftIO $ isRepo (projectPathF </> ".git")
+        setTitle $ toHtml $ "Hit - " ++ (T.unpack projectName)
+        $(widgetFile "default-head")
+        if not isHitProject
+            then error $ "No such project: " ++ (T.unpack projectName)
+            else do
+                let identityNew = T.unpack identityTree
+                treeList <- liftIO $ withRepo (projectPathF </> ".git") $ myGetTreeMaybe $ fromHexString $ T.unpack ref
+                $(widgetFile "project-show-tree")
