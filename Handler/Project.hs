e7f79824b563f09b4c897850ba14522c476ec946
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-01-29 21:24:26 +0000

    Handle README.md
    Extract the content of README.md file from the Master Tree.
    Convert the Markdown to Html and print it on the project page

--- Handler/Project.hs
+++ Handler/Project.hs
@@ -5,15 +5,19 @@ import Data.List as L (head, init, tail)
 import Data.List.Split as L (splitOn)
 import Data.Text as T (unpack, pack)
 
+import Yesod.Markdown
+
 import Data.Git
 import Data.Git.Types
 import Data.Git.Storage
+import Data.Git.Storage.Object
 import Data.Git.Revision
 
 import Data.Time.Format
 import System.Locale
 
 import Data.ByteString.Char8 as BC (unpack)
+import Data.ByteString.Lazy.Char8 as BL
 
 getLogList :: Revision -> Data.Git.Storage.Git -> IO [(Commit,Ref)]
 getLogList revision git = do
@@ -27,6 +31,20 @@ getLogList revision git = do
                   []    -> return $ list ++ [(commit,ref)]
                   (p:_) -> loopTill (i-1) p (list ++ [(commit,ref)])
 
+getReadmeContent :: Revision -> Data.Git.Storage.Git -> IO (Maybe Html)
+getReadmeContent rev git = do
+    ref <- maybe (error "revision cannot be found") id <$> resolveRevision git rev
+    readmeRef <- resolvePath git ref ["README.md"]
+    case readmeRef of
+        Nothing  -> return Nothing
+        Just bRef -> do
+            contentMaybe <- getObjectRaw git bRef True
+            case contentMaybe of
+                Nothing      -> return Nothing
+                Just content ->
+                    let mdContent = Markdown $ T.pack $ BL.unpack $ oiData content
+                    in  return $ Just $ markdownToHtmlTrusted mdContent
+
 -- This is the project summary handler.
 getProjectR :: Text -> Handler Html
 getProjectR projectName  = do
@@ -34,6 +52,7 @@ getProjectR projectName  = do
     defaultLayout $ do
         identityDescription <- newIdent
         identityLogList <- newIdent
+        identityReadme <- newIdent
         setTitle $ toHtml $ "Hit - " ++ (T.unpack projectName)
         hitProjectPath <- liftIO $ getProjectPath (extraProjectsDir extra) projectName
         case hitProjectPath of
@@ -41,4 +60,5 @@ getProjectR projectName  = do
             Just path -> do
                 commits     <- liftIO $ withRepo path $ getLogList (fromString "master")
                 description <- liftIO $ withRepo path $ getDescription
+                readmeMD    <- liftIO $ withRepo path $ getReadmeContent (fromString "master")
                 $(widgetFile "project")
