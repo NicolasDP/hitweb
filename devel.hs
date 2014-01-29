ea0e8664de999f5d50fcaead3c012a7822059c58
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-01-29 15:01:31 +0000

    rename the project

--- devel.hs
+++ devel.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE PackageImports #-}
-import "hithub" Application (getApplicationDev)
+import "hitweb" Application (getApplicationDev)
 import Network.Wai.Handler.Warp
     (runSettings, defaultSettings, settingsPort)
 import Control.Concurrent (forkIO)
