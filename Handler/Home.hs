a6676722ab8b83fe269b2bed4605c2897245e7cd
Nicolas DI PRIMA <nicolas@di-prima.fr>
2014-01-30 20:03:40 +0000

    Move the getProjects function from Home.hs

--- Handler/Home.hs
+++ Handler/Home.hs
@@ -1,30 +1,12 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE OverloadedStrings #-}
 module Handler.Home where
 
 import Import
-
-import Data.Git.Storage (isRepo)
-
-import Filesystem.Path as FSP
-import Filesystem.Path.CurrentOS as FSP
-import Filesystem
-
-getProjectsName :: Text -> IO [Text]
-getProjectsName (FSP.fromText -> hitwebPath) =
-    foldr f (return []) =<< listDirectory hitwebPath
-  where f :: FSP.FilePath -> IO [Text] -> IO [Text]
-        f t accu = do
-            let gitPath = hitwebPath </> t </> ".git"
-            isHitProject <- isRepo gitPath
-            if isHitProject then (either id id (FSP.toText t):) <$> accu
-                            else accu
 
 getHomeR :: Handler Html
 getHomeR = do
     extra <- getExtra
     defaultLayout $ do
         aDomId <- newIdent
-        projectsList <- liftIO $ getProjectsName $ extraProjectsDir extra
+        projectsList <- liftIO $ listProjectIn $ extraProjectsDir extra
         setTitle "Home Page"
         $(widgetFile "homepage")
--- Hitweb/Filepath.hs
+++ Hitweb/Filepath.hs
@@ -1,24 +1,35 @@
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Hitweb.Filepath
     ( getProjectPath
+    , listProjectIn
     ) where
 
 import Prelude
+import Control.Applicative
 
 import Data.Git.Storage
 
-import Data.Text as T (Text, unpack)
+import Data.Text as T (Text)
 
+import Filesystem
 import Filesystem.Path as FSP
-import Filesystem.Path.Rules as FSP
+import Filesystem.Path.CurrentOS as FSP
 
 -- | for the hitweb projects directory (dir), return the path to the git
 -- directory:
 --   Just dir/projectName (is a git directory - project.git)
 --   Just git/projectName/.git (is a git directory)
 --   Nothing
+--
 getProjectPath :: Text -> Text -> IO (Maybe FSP.FilePath)
-getProjectPath dir projectName = do
-    let path1 = (toPath dir) </> (toPath projectName)
+getProjectPath (FSP.fromText -> dir)
+               (FSP.fromText -> projectName) =
+    getProjectPath' dir projectName
+
+getProjectPath' :: FSP.FilePath -> FSP.FilePath -> IO (Maybe FSP.FilePath)
+getProjectPath' dir projectName = do
+    let path1 = dir </> projectName
     let path2 = path1 </> ".git"
     isGitProj1 <- isRepo path1
     isGitProj2 <- isRepo path2
@@ -26,5 +37,14 @@ getProjectPath dir projectName = do
         (True , _   ) -> Just path1
         (False, True) -> Just path2
         _             -> Nothing
-    where toPath :: Text -> FSP.FilePath
-          toPath path = FSP.decodeString posix_ghc704 $ T.unpack path
+
+-- | list all of the current projects in this root directory
+listProjectIn :: Text -> IO [Text]
+listProjectIn (FSP.fromText -> rootPath) =
+    foldr f (return []) =<< listDirectory rootPath
+  where f :: FSP.FilePath -> IO [Text] -> IO [Text]
+        f t accu = do
+            pathMaybe <- getProjectPath' rootPath t
+            case pathMaybe of
+                Just _  -> (either id id (FSP.toText $ FSP.filename t):) <$> accu
+                Nothing -> accu
